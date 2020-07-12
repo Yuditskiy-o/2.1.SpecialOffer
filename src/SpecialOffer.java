bbfb6d2b51c94b2fad9f88cc46ab2ce76ee7629f
Yuditskiy-o <Komekadze@gmail.com>
2020-07-12 22:09:13 +0700

    SpecialOffer.java modified

--- src/SpecialOffer.java
+++ src/SpecialOffer.java
@@ -1,5 +1,16 @@
 public class SpecialOffer {
     public static void main(String[] args) {
-        System.out.println();
+        long transactionInKopecks = 150000;
+        long bonusPointsInKopecks;
+        long valueInKopecks = 100000;
+        if (transactionInKopecks > valueInKopecks) {
+            bonusPointsInKopecks = transactionInKopecks / 10000;
+        } else {
+            bonusPointsInKopecks = 0;
+        }
+        long balanceInKopecks = 500000;
+        long totalBalanceInKopecks = (balanceInKopecks + transactionInKopecks + bonusPointsInKopecks) / 100;
+        System.out.println("Всего бонусных баллов — " + bonusPointsInKopecks);
+        System.out.println("Всего средств на счету — " + totalBalanceInKopecks);
     }
 }
